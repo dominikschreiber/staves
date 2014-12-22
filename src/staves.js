5e101e302e48a5a1eb1a8e63d120e0d76dc7a1b4
Dominik Schreiber <domischreib@gmail.com>
2014-12-22 01:08:58 +0100

    removed parsing/converting/rendering of staves/vextab/pdf; added modules parser/converter/renderer for that; added stream-based interface (actually not really streamed as es.wait() is called at first)

--- src/staves.js
+++ src/staves.js
@@ -1,75 +1,21 @@
-process.env.PWD = require('phantomjs').path;
-
-function parseStavesContent(raw) {
-  var split = raw
-    .replace(/\n#[^\n]*\n/g, '\n') // remove # comments
-    .split('\n\n');
-  
-  return {
-    headline: split[0],
-    notes: split[1].split('\n'),
-    texts: _.map(split.slice(2), function(verse) { return verse.split('\n'); })
-  }
-}
-
-function createVextabs(parsed, stavesPerVextab) {
-  return _.map(subarraysOfLength(_.flatten(_.map(parsed.texts, function(verse) {
-      return _.map(_.zip(parsed.notes, verse), function(noteVerse) {
-        return [
-          parsed.headline,
-          'notes ' + noteVerse[0],
-          'text ++,.11,.font=Arial-14-normal,' + noteVerse[1]
-        ].join('\n');
-      });
-    })), stavesPerVextab), function(page) {
-    return 'options space=0 stave-distance=40\n\n' + page.join('\n\n');
-  });
-}
-
-function subarraysOfLength(array, length) {
-  var subarrays = [[]];
-  for (var i = 0; i < array.length; i++) {
-    if (subarrays[subarrays.length - 1].length < length) {
-      subarrays[subarrays.length - 1].push(array[i]);
-    } else {
-      subarrays.push([array[i]]);
-    }
-  }
-  return subarrays;
-}
-
-function createHtmlForVextab(vextab, scripts) {
-  return '<html>' + 
-    '<head>' + _.map(scripts, function(script) { return '<script>' + script + '</script>'; }).join('') + '</head>' + 
-    '<body style="-webkit-filter: invert(100%)"><div class="vex-tabdiv" width=680 scale=1.0 editor=false>' + vextab + '</div></body>' + 
-    '</html>';
-}
-
 var fs = require('fs')
-  , _ = require('underscore')
-  , _s = require('underscore.string')
-  , htmlpdf = require('html-pdf')
+  , es = require('event-stream')
 
-  , vextabDivJs = fs.readFileSync(__dirname + '/../node_modules/vextab/releases/vextab-div.js')
-  , raphaelJs = fs.readFileSync(__dirname + '/../node_modules/raphael/raphael-min.js');
+  , parser = require('./parser')
+  , converter = require('./converter')
+  , renderer = require('./renderer');
 
 module.exports = function(program) {
-  var infile = program.I
-    , outfile = program.O
-    , transpose = program.T
-    , width = program.W
-    , height = program.H;
-  
-  fs.readFile(infile, function(err, buff) {
-    var content = parseStavesContent(buff.toString())
-      , vextabs = createVextabs(content, Math.floor(height / 250));
-    
-    _.each(vextabs, function(vextab, index) {
-      htmlpdf.create(createHtmlForVextab(vextab, [raphaelJs, vextabDivJs]), {
-        width: width + 'px',
-        height: height + 'px',
-        filename: outfile.replace('.pdf', '-' + (index + 1) + '.pdf')
-      }, function(err, buffer) {});
-    });
-  });
+    var infile = program.I
+      , outfile = program.O
+      , transpose = program.T
+      , width = program.W
+      , height = program.H
+      , shouldUseSvg = program.S;
+
+    fs.createReadStream(infile)
+        .pipe(es.wait())
+        .pipe(parser.parse())
+        .pipe(converter.toVextabs(Math.floor(height / 250)))
+        .pipe(renderer.toPdf(outfile, shouldUseSvg, width, height));
 };
