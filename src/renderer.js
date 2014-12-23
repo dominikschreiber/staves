3d8dd16c6de353cac93699371a39ba07a6f4ced5
Dominik Schreiber <domischreib@gmail.com>
2014-12-23 13:38:35 +0100

    added 0-padded indices to filenames; added minimal stylesheet to generated html

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
   , es = require('event-stream')
   , _ = require('underscore')
+  , _s = require('underscore.string')
   , htmlpdf = require('html-pdf');
 
 exports.toPdf = function(outfile, shouldUseSvg, width, height) {
@@ -12,23 +13,33 @@ exports.toPdf = function(outfile, shouldUseSvg, width, height) {
     scripts.push(fs.readFileSync(__dirname + '/../node_modules/vextab/releases/vextab-div.js'));
     
     return es.mapSync(function(vextabs) {
-        _.each(vextabs, function(vextab, index) {
-            htmlpdf.create(createHtmlForVextab(vextab, scripts), {
-                width: width + 'px',
-                height: height + 'px',
-                filename: outfile.replace('.pdf', '-' + (index + 1) + '.pdf')
-            }, function(err, buffer) {});
+        var filenames = _.map(_.range(vextabs.length), function(index) {
+           return outfile.replace('.pdf', '-' + _s.lpad(index + 1, 2, '0') + '.pdf');
         });
+        
+        _.chain(vextabs)
+            .zip(filenames)
+            .each(function(vextabFilename) {
+                htmlpdf.create(createHtmlForVextab(vextabFilename[0], scripts), {
+                    width: width + 'px',
+                    height: height + 'px',
+                    filename: vextabFilename[1]
+                }, function(err, buffer) {});
+            });
+        
+        return filenames;
     });
 };
 
 function createHtmlForVextab(vextab, scripts) {
     return '<html>' +
-        '<head>' + _.chain(scripts)
-            .filter(function(script) { return script; })
-            .map(function(script) { return '<script>' + script + '</script>'; })
-            .value()
-            .join('') +
+        '<head>' + 
+            '<style>body{display:flex;height:100vh}body>*{margin:auto}</style>' + 
+            _.chain(scripts)
+                .filter(function(script) { return script; })
+                .map(function(script) { return '<script>' + script + '</script>'; })
+                .value()
+                .join('') +
         '</head>' +
         '<body>' +
             '<div class="vex-tabdiv" width=680 scale=1.0 editor=false>' + vextab + '</div>' +
