07eb9b3f7486dc10c6ee277582cdb917d016a2ee
Dominik Schreiber <domischreib@gmail.com>
2014-12-23 15:41:59 +0100

    added debug output when called with DEBUG=parsed

--- src/parser.js
+++ src/parser.js
@@ -1,17 +1,25 @@
 var es = require('event-stream')
-  , _ = require('underscore');
+  , _ = require('underscore')
+  , debug = require('debug')('parsed');
 
 exports.parse = function() {
     return es.mapSync(function (raw) {
         var split = raw
             .toString()
             .replace(/\n#[^\n]*\n/g, '\n') // remove # comments
-            .split('\n\n');
-
-        return {
-            headline: split[0],
-            notes: split[1].split('\n'),
-            texts: _.map(split.slice(2), function(verse) { return verse.split('\n'); })
-        };
+            .split('\n\n')
+        
+          , parsed = {
+                headline: split[0],
+                notes: split[1].split('\n'),
+                texts: _.map(split.slice(2), function(verse) {
+                    return verse
+                        .replace(/ /g, ',')
+                        .split('\n');
+                })
+            };
+        
+        debug(parsed);
+        return parsed;
     });
 };
