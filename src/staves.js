dd5aab2ffed986fb3943368a5308dc0da7e1eea7
Dominik Schreiber <domischreib@gmail.com>
2014-12-23 13:37:38 +0100

    added stavesPerPage computation for viewport relations (found that a relation below width/height=1.55 should create 3 staves per page, less should create 2); added filename extraction (foo.staves -> foo.pdf)

--- src/staves.js
+++ src/staves.js
@@ -5,16 +5,25 @@ var fs = require('fs')
   , converter = require('./converter')
   , renderer = require('./renderer');
 
+function stavesPerPage(width, height) {
+    // 3.9 is empirically found
+    return Math.round(3.9 / (width / height));
+}
+
 module.exports = function(program) {
     var infile = program.I
       , outfile = program.O
       , width = program.W
       , height = program.H
       , shouldUseSvg = program.S;
+    
+    if (!outfile) {
+        outfile = infile.substring(infile.lastIndexOf('/') + 1).replace('.staves', '.pdf');
+    }
 
     fs.createReadStream(infile)
         .pipe(es.wait())
         .pipe(parser.parse())
-        .pipe(converter.toVextabs(Math.floor(height / 250)))
+        .pipe(converter.toVextabs(stavesPerPage(width, height)))
         .pipe(renderer.toPdf(outfile, shouldUseSvg, width, height));
 };
